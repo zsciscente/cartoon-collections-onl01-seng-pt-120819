e4b45c393b78784d1b1423df89571ab4965e2e44
Zachary Sciscente <zsciscente@gmail.com>
2019-12-22 16:01:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,9 +15,9 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls_long)
-  if calls_long.length > 3
+  if calls_long.length >= 3
     true
-  else calls_long.length < 4
+  else 
     false
 end
   
